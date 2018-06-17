322ee447269270aab8b5ad8d80149b8d6fc37d7f
jhars <jonharlan.io@gmail.com>
2018-06-17 05:29:28 -0400

    Relative Path for model_sequelizer

--- database/model_sequelizer.js
+++ database/model_sequelizer.js
@@ -4,30 +4,28 @@ const path = require('path');
 const Sequelize = require('sequelize');
 const basename = path.basename(module.filename);
 const env = process.env.NODE_ENV || 'development';
-const config_env = require(`./config.json`)[env];
+const config = require(`./config.json`)[env];
 const db = {};
-const models_dir = '/Users/macpan/dev/APPS/GiGi/API/GiGi-API/server/models'
+const models_dir = '../server/models/'
 
 
 let sequelize;
-if (config_env.use_env_variable) {
-  sequelize = new Sequelize(process.env[config_env.use_env_variable]);
+if (config.use_env_variable) {
+  sequelize = new Sequelize(process.env[config.use_env_variable]);
 } else {
   sequelize = new Sequelize(
-    config_env.database, config_env.username, config_env.password, config_env
+    config.database, config.username, config.password, config
   );
 }
 
-console.log('PATH.basename... new: ' + path.basename(models_dir));
-
 fs
-  .readdirSync('/Users/macpan/dev/APPS/GiGi/API/GiGi-API/server/models')
+  .readdirSync(process.env.PWD + '/server/models')
   .filter((file) =>
     (file.indexOf('.') !== 0) &&
     (file !== 'index.js') &&
     (file.slice(-3) === '.js'))
   .forEach((file) => {
-    const model = sequelize.import(models_dir + '/' + file);
+    const model = sequelize.import(models_dir + file);
     db[model.name] = model;
   });
 
